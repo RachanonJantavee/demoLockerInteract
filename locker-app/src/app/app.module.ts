47602928a8ffc173fb97273bddf9044200b3ea66
TL3 <rachanonjantavee@gmail.com>
2023-10-18 12:26:12 +0700

    create homepage components and routing to default page

--- locker-app/src/app/app.module.ts
+++ locker-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationBarComponent } from './homepage/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './homepage/footer/footer.component';
+import { HeroSectionComponent } from './homepage/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavigationBarComponent,
+    FooterComponent,
+    HeroSectionComponent,
   ],
   imports: [
     BrowserModule,
